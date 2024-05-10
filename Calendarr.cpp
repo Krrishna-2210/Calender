dcb95cca0b4c206a17371da83677b8aff2af9210
HETVI <148623641+Hetvi2211@users.noreply.github.com>
2024-05-10 11:50:59 +0530

    Update Calendarr.cpp

--- Calendarr.cpp
+++ Calendarr.cpp
@@ -15,9 +15,9 @@ public:
 
        int getDaysInMonth()
        {
-         if(month == 2)
+         if (month ==2)
          {
-           if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+           if ((year % 4 == 0 && year % 100 != 0) || (year % 400 ==0))
              return 29;  //Leap year
            else
              return 28;//Non-leap year
